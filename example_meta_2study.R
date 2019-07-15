746d569178b4ce466812df8b70630a194a09d1cb
bailliem <bailliem@gmail.com>
2019-07-15 23:59:36 +0200

    better set up meta analysis with two studies. inferetial and summmary statistics

---
+++ example_meta_2study.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(gt)
+
+### READ IN ARD - Simple 2 study demog example
+ARD <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTwUHjU4ZMYQwwCGnO3xlkd0oJbOVdt20YuzWGr2XkQaSyrDcYJyWv-zx5fVMAN38A7k_4js10BlN_t/pub?output=csv")
+
+ARD %>% glimpse()
+
+### FILTER ON All cause mortality FOR BOTH STUDIES
+ACM <- ARD %>%
+  filter(PARAMCD == "ACM" & ANALTYP1 == "COMPARISON")
+
+ACM %>% glimpse()
+
+## Arrange in to table format
+T1 <- 
+  ACM %>% 
+  select(STUDYID, TRTVAL, STAT, STATVAL) %>%
+  group_by(STUDYID, TRTVAL, STAT) %>%
+  mutate(id = row_number()) %>% 
+  gather(groupname, value, -id, -STUDYID, -TRTVAL, -STAT) %>% 
+  spread(STAT, value)
+
+
+## Simple plot
+T1 %>% ggplot(aes(x = TRTVAL, y = HR)) +
+  geom_pointrange(aes(ymin = LOWER95, 
+                      ymax = UPPER95), 
+                  alpha = 0.7, color = "#c0392b", size = 0.25) +
+  coord_flip() +
+  facet_wrap(~STUDYID, ncol=1) + 
+  theme_minimal()
