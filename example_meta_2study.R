a2624a97acf476e4e6bf21b58de24f85586cd724
bailliem <bailliem@gmail.com>
2019-07-16 00:03:42 +0200

    first disply n/N and then inferential stat. TODO, combine in to one output

--- example_meta_2study.R
+++ example_meta_2study.R
@@ -6,14 +6,33 @@ ARD <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTwUHjU4ZMYQwwC
 
 ARD %>% glimpse()
 
-### FILTER ON All cause mortality FOR BOTH STUDIES
+
+
+### FILTER ON All cause mortality FOR BOTH STUDIES - summary statistics
+ACMsum <- ARD %>%
+  filter(PARAMCD == "ACM" & ANALTYP1 == "SUMMARY")
+
+ACMsum %>% glimpse()
+
+## Arrange in to table format
+T1 <- 
+  ACMsum %>% 
+  select(STUDYID, TRTVAL, STAT, STATVAL) %>%
+  group_by(STUDYID, TRTVAL, STAT) %>%
+  mutate(id = row_number()) %>% 
+  gather(groupname, value, -id, -STUDYID, -TRTVAL, -STAT) %>% 
+  spread(STAT, value)
+
+T1
+
+### FILTER ON All cause mortality FOR BOTH STUDIES - inferntial statistics
 ACM <- ARD %>%
   filter(PARAMCD == "ACM" & ANALTYP1 == "COMPARISON")
 
 ACM %>% glimpse()
 
 ## Arrange in to table format
-T1 <- 
+T2 <- 
   ACM %>% 
   select(STUDYID, TRTVAL, STAT, STATVAL) %>%
   group_by(STUDYID, TRTVAL, STAT) %>%
@@ -23,10 +42,13 @@ T1 <-
 
 
 ## Simple plot
-T1 %>% ggplot(aes(x = TRTVAL, y = HR)) +
+T2 %>% ggplot(aes(x = TRTVAL, y = HR)) +
   geom_pointrange(aes(ymin = LOWER95, 
                       ymax = UPPER95), 
                   alpha = 0.7, color = "#c0392b", size = 0.25) +
   coord_flip() +
   facet_wrap(~STUDYID, ncol=1) + 
   theme_minimal()
+
+
+
